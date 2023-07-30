311ed5585807e25d1e2d93cdb35371172b2033ac
ELIEDSON GUSTAVO <83400143+ELIEDSON-GUSTAVO@users.noreply.github.com>
2023-07-30 15:04:06 -0300

    push

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Script para transição suave de páginas
+// Função para fazer a transição de páginas suavemente
 function smoothScroll(target, duration) {
   var target = document.querySelector(target);
     var targetPosition = target.getBoundingClientRect().top;
@@ -24,17 +24,37 @@ function smoothScroll(target, duration) {
                                                         requestAnimationFrame(animation);
                                                         }
 
-                                                        var btnScroll = document.querySelector(".btn-scroll");
-                                                        btnScroll.addEventListener("click", function () {
-                                                          smoothScroll("#produtos", 1000);
-                                                          });
+                                                        // Event listeners para as categorias do menu
+                                                        var categoriaList = document.querySelector('.categoria-list');
+                                                        categoriaList.addEventListener('click', function (event) {
+                                                          if (event.target.tagName === 'A') {
+                                                              event.preventDefault();
+                                                                  var target = event.target.getAttribute('href');
+                                                                      smoothScroll(target, 1000);
+                                                                        }
+                                                                        });
 
-                                                          var categoriaLinks = document.querySelectorAll(".categoria-list a");
-                                                          categoriaLinks.forEach(function (link) {
-                                                            link.addEventListener("click", function (e) {
-                                                                e.preventDefault();
-                                                                    var target = e.target.getAttribute("href");
-                                                                        smoothScroll(target, 1000);
-                                                                          });
-                                                                          });
-                                                                          
+                                                                        // Event listener para o botão de "Comprar" em cada produto
+                                                                        var btnComprarList = document.querySelectorAll('.btn-cart');
+                                                                        btnComprarList.forEach(function (btn) {
+                                                                          btn.addEventListener('click', function () {
+                                                                              var productDetails = btn.parentElement;
+                                                                                  var productName = productDetails.querySelector('h3').innerText;
+                                                                                      var productPrice = productDetails.querySelector('.product-price').innerText;
+
+                                                                                          // Aqui você pode fazer alguma ação com os dados do produto (como adicionar ao carrinho)
+                                                                                              // Por exemplo, você pode enviar esses dados para o backend ou mostrar uma mensagem de sucesso
+                                                                                                  console.log('Nome do Produto:', productName);
+                                                                                                      console.log('Preço do Produto:', productPrice);
+                                                                                                        });
+                                                                                                        });
+
+                                                                                                        // Event listeners para as páginas de descrição de produtos
+                                                                                                        var productItemList = document.querySelectorAll('.product-item');
+                                                                                                        productItemList.forEach(function (item) {
+                                                                                                          item.addEventListener('click', function () {
+                                                                                                              var productDetails = item.querySelector('.product-details');
+                                                                                                                  productDetails.classList.toggle('show');
+                                                                                                                    });
+                                                                                                                    });
+                                                                                                                    
